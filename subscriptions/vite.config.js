dbc06185cc736d1480a1b78343f7d1fba68d6e4d
TriUbi <triubilla@gmail.com>
2025-01-31 10:30:06 +0100

    Simplify configuration and fix paths

--- subscriptions/vite.config.js
+++ subscriptions/vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,16 +7,5 @@ export default defineConfig({
   base: "/Subscription_tracker/",
   build: {
     outDir: "dist",
-    assetsDir: "assets",
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, "index.html"),
-      },
-    },
-  },
-  resolve: {
-    alias: {
-      "@": resolve(__dirname, "./src"),
-    },
   },
 });
